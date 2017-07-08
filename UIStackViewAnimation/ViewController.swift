50d64e7ea1f7753d0ccbb61053e84b9fbd04935a
adhikarivijay05 <adhikari.vijay05@example.comgit config --global user.email adhikari.vijay05@example.com>
2017-07-08 23:47:09 +0530

    UIStackeView animation

--- UIStackViewAnimation/ViewController.swift
+++ UIStackViewAnimation/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: [UIView]!
+        {
+        didSet{
+            colorView.forEach {
+                $0.isHidden = true
+            }
+        }
+    }
+    
+    
+    @IBOutlet weak var selectedView: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +33,48 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func horseClicked(_ sender: Any) {
+        hideViews(selectedString: "🐎")
+    }
+    @IBAction func pigClicked(_ sender: Any) {
+        hideViews(selectedString: "🐖")
 
+    }
 
+    @IBAction func mouseClicked(_ sender: Any) {
+        hideViews(selectedString: "🐁")
+
+    }
+    @IBAction func tigerClicked(_ sender: Any) {
+        hideViews(selectedString: "🐅")
+
+    }
+    @IBAction func sankeClicked(_ sender: Any) {
+        hideViews(selectedString: "🐍")
+
+    }
+    @IBAction func camelClicked(_ sender: Any) {
+        hideViews(selectedString: "🐪")
+
+    }
+
+    @IBAction func butterflyClicked(_ sender: Any) {
+        hideViews(selectedString: "🦋")
+
+    }
+    @IBAction func goatClicked(_ sender: Any) {
+        hideViews(selectedString: "🐑")
+    }
+    
+    
+    
+    func hideViews(selectedString : String){
+            UIView.animate(withDuration: 0.3) {
+                self.colorView.forEach{
+                $0.isHidden = !$0.isHidden
+                }
+                self.selectedView.text = selectedString
+        }
+    }
 }
 
